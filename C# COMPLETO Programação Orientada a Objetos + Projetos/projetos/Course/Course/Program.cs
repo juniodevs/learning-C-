8168f31fff5dec23ccad477fd5fb3106d49ee81f
Júnior Veras <iramarjunhorveras@gmail.com>
2022-09-23 11:02:12 -0300

    23. Entrada de dados em C# - PARTE 2

--- C# COMPLETO Programação Orientada a Objetos + Projetos/projetos/Course/Course/Program.cs
+++ C# COMPLETO Programação Orientada a Objetos + Projetos/projetos/Course/Course/Program.cs
@@ -5,26 +5,42 @@ namespace Course
     internal class Program {
         static void Main(string[] args) {
 
+            int n1 = int.Parse(Console.ReadLine());
+            char ch = char.Parse(Console.ReadLine());
+            double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            string frase = Console.ReadLine();
-            string x = Console.ReadLine();
-            string y = Console.ReadLine();
-            string z = Console.ReadLine();
             string[] vet = Console.ReadLine().Split(' ');
-            string a = vet[0];
-            string b = vet[1];
-            string c = vet[2];
-
+            string nome = vet[0];
+            char sexo = char.Parse(vet[1]);
+            int idade = int.Parse(vet[2]);
+            double altura = double.Parse(vet[3], CultureInfo.InvariantCulture);
 
             Console.WriteLine("Você Digitou: ");
-            Console.WriteLine(frase);
-            Console.WriteLine(x);
-            Console.WriteLine(y);
-            Console.WriteLine(z);
-            Console.WriteLine(a);
-            Console.WriteLine(b);
-            Console.WriteLine(c);
+            Console.WriteLine(n1);
+            Console.WriteLine(ch);
+            Console.WriteLine(n2.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(nome);
+            Console.WriteLine(sexo);
+            Console.WriteLine(altura);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
 
+            //string frase = Console.ReadLine();
+            //string x = Console.ReadLine();
+            //string y = Console.ReadLine();
+            //string z = Console.ReadLine();
+            //string[] vet = Console.ReadLine().Split(' ');
+            //string a = vet[0];
+            //string b = vet[1];
+            //string c = vet[2];
+            //Console.WriteLine("Você Digitou: ");
+            //Console.WriteLine(frase);
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);
+            //Console.WriteLine(z);
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+            //Console.WriteLine(c);
             //int n1 = 3 + 4 * 2;
             //int n2 = (3 + 4) * 2;
             //int n3 = 17 % 3;
@@ -49,7 +65,7 @@ namespace Course
             //Console.WriteLine(x1);
             //Console.WriteLine(x2);
 
-            
+
             //float x = 4.5f;
             //double y = x;
             //Console.WriteLine(y);
