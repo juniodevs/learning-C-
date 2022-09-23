e4ea98fbb144a6a5bad79491029c8517b3b41516
Júnior Veras <iramarjunhorveras@gmail.com>
2022-09-23 10:31:58 -0300

    21. Operadores aritméticos

--- C# COMPLETO Programação Orientada a Objetos + Projetos/projetos/Course/Course/Program.cs
+++ C# COMPLETO Programação Orientada a Objetos + Projetos/projetos/Course/Course/Program.cs
@@ -5,24 +5,49 @@ namespace Course
     internal class Program {
         static void Main(string[] args) {
 
-            float x = 4.5f;
-            double y = x;
-            Console.WriteLine(y);
+            int n1 = 3 + 4 * 2;
+            int n2 = (3 + 4) * 2;
+            int n3 = 17 % 3;
+            double n4 = 10.0 / 8; 
+            //OU
+            double n5 = (double)10 / 8;
 
-            double a = 5.1;
-            float b;
-            b = (float)a;
-            Console.WriteLine(b);
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+            Console.WriteLine(n5);
 
-            int c = (int)a;
-            Console.WriteLine(c);
+            double a = 1.0, b = -3.0, c = -4.0;
 
-            int d = 5;
-            int e = 2;
+            double delta = Math.Pow(b, 2.0) - 4.0 * a * c;
 
-            double resultado = (double)d / e;
+            double x1 = (-b + Math.Sqrt(delta)) / (2.0 * a);
+            double x2 = (-b - Math.Sqrt(delta)) / (2.0 * a);
 
-            Console.WriteLine(resultado);
+            Console.WriteLine(delta);
+            Console.WriteLine(x1);
+            Console.WriteLine(x2);
+
+            
+            //float x = 4.5f;
+            //double y = x;
+            //Console.WriteLine(y);
+
+            //double a = 5.1;
+            //float b;
+            //b = (float)a;
+            //Console.WriteLine(b);
+
+            //int c = (int)a;
+            //Console.WriteLine(c);
+
+            //int d = 5;
+            //int e = 2;
+
+            //double resultado = (double)d / e;
+
+            //Console.WriteLine(resultado);
 
             //int a = 10;
             //Console.WriteLine(a);
