419b0e5a2a1042a3cd54d8f7623465d1d94892f3
Júnior Veras <iramarjunhorveras@gmail.com>
2022-09-23 10:48:02 -0300

    22. Entrada de dados em C# - PARTE 1

--- C# COMPLETO Programação Orientada a Objetos + Projetos/projetos/Course/Course/Program.cs
+++ C# COMPLETO Programação Orientada a Objetos + Projetos/projetos/Course/Course/Program.cs
@@ -5,29 +5,49 @@ namespace Course
     internal class Program {
         static void Main(string[] args) {
 
-            int n1 = 3 + 4 * 2;
-            int n2 = (3 + 4) * 2;
-            int n3 = 17 % 3;
-            double n4 = 10.0 / 8; 
-            //OU
-            double n5 = (double)10 / 8;
 
-            Console.WriteLine(n1);
-            Console.WriteLine(n2);
-            Console.WriteLine(n3);
-            Console.WriteLine(n4);
-            Console.WriteLine(n5);
+            string frase = Console.ReadLine();
+            string x = Console.ReadLine();
+            string y = Console.ReadLine();
+            string z = Console.ReadLine();
+            string[] vet = Console.ReadLine().Split(' ');
+            string a = vet[0];
+            string b = vet[1];
+            string c = vet[2];
 
-            double a = 1.0, b = -3.0, c = -4.0;
 
-            double delta = Math.Pow(b, 2.0) - 4.0 * a * c;
+            Console.WriteLine("Você Digitou: ");
+            Console.WriteLine(frase);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
 
-            double x1 = (-b + Math.Sqrt(delta)) / (2.0 * a);
-            double x2 = (-b - Math.Sqrt(delta)) / (2.0 * a);
+            //int n1 = 3 + 4 * 2;
+            //int n2 = (3 + 4) * 2;
+            //int n3 = 17 % 3;
+            //double n4 = 10.0 / 8; 
+            ////OU
+            //double n5 = (double)10 / 8;
 
-            Console.WriteLine(delta);
-            Console.WriteLine(x1);
-            Console.WriteLine(x2);
+            //Console.WriteLine(n1);
+            //Console.WriteLine(n2);
+            //Console.WriteLine(n3);
+            //Console.WriteLine(n4);
+            //Console.WriteLine(n5);
+
+            //double a = 1.0, b = -3.0, c = -4.0;
+
+            //double delta = Math.Pow(b, 2.0) - 4.0 * a * c;
+
+            //double x1 = (-b + Math.Sqrt(delta)) / (2.0 * a);
+            //double x2 = (-b - Math.Sqrt(delta)) / (2.0 * a);
+
+            //Console.WriteLine(delta);
+            //Console.WriteLine(x1);
+            //Console.WriteLine(x2);
 
             
             //float x = 4.5f;
