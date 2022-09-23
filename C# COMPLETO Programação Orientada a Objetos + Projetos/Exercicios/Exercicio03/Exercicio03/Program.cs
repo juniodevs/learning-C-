3e2dd7e3fa401d03bde1cda88cc965e1f674bcf8
Júnior Veras <iramarjunhorveras@gmail.com>
2022-09-23 11:46:42 -0300

    Exercício 03

---
+++ C# COMPLETO Programação Orientada a Objetos + Projetos/Exercicios/Exercicio03/Exercicio03/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ Fazer um programa para ler quatro valores inteiros A, B, C e D.
+ A seguir, calcule e mostre a diferença do produto 
+ de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).
+*/
+
+using System;
+
+namespace Program
+{
+    internal class Program {
+        static void Main(string[] args) {
+
+
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+            int diferenca = (a * b - c * d);
+
+            Console.WriteLine("DIFERENCA = " + diferenca);
+
+        }
+    }
+}
