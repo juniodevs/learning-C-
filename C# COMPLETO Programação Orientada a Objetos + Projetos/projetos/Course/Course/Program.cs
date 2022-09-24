8e3f82ee06906e20f70f6a0e8d0827035664476a
Júnior Veras <iramarjunhorveras@gmail.com>
2022-09-24 20:56:33 -0300

    26. Operadores lógicos

--- C# COMPLETO Programação Orientada a Objetos + Projetos/projetos/Course/Course/Program.cs
+++ C# COMPLETO Programação Orientada a Objetos + Projetos/projetos/Course/Course/Program.cs
@@ -6,28 +6,40 @@ namespace Course
         static void Main(string[] args) {
 
 
-            int a = 10;
+            bool c1 = 2 > 3 && 4 != 5;
+            bool c2 = 2 > 3 || 4 != 5; // true
+            bool c3 = !(2 > 3) && 4 != 5; // true
 
-            bool c1 = a < 10;
-            bool c2 = a < 20;
-            bool c3 = a > 10;
-            bool c4 = a > 5;
-            
             Console.WriteLine(c1);
             Console.WriteLine(c2);
             Console.WriteLine(c3);
-            Console.WriteLine(c4);
-            Console.WriteLine("--------------");
 
-            bool c5 = a <= 10;
-            bool c6 = a >= 10;
-            bool c7 = a == 10;
-            bool c8 = a != 10;
+            Console.WriteLine("-----");
+
+            bool c4 = 10 < 5; // false
+            bool c5 = c2 || c3 && c4;
 
             Console.WriteLine(c5);
-            Console.WriteLine(c6);
-            Console.WriteLine(c7);
-            Console.WriteLine(c8);
+
+
+            //int a = 10;
+            //bool c1 = a < 10;
+            //bool c2 = a < 20;
+            //bool c3 = a > 10;
+            //bool c4 = a > 5;
+            //Console.WriteLine(c1);
+            //Console.WriteLine(c2);
+            //Console.WriteLine(c3);
+            //Console.WriteLine(c4);
+            //Console.WriteLine("--------------");
+            //bool c5 = a <= 10;
+            //bool c6 = a >= 10;
+            //bool c7 = a == 10;
+            //bool c8 = a != 10;
+            //Console.WriteLine(c5);
+            //Console.WriteLine(c6);
+            //Console.WriteLine(c7);
+            //Console.WriteLine(c8);
 
             //Console.WriteLine("Entre com seu nome completo: ");
             //string nome = Console.ReadLine();
