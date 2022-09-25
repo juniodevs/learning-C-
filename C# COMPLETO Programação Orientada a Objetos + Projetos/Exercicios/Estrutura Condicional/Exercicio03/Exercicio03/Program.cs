7a1262562a0c5e847471ae84f0a581219b1eef99
Júnior Veras <iramarjunhorveras@gmail.com>
2022-09-25 15:41:44 -0300

    Exercício 03

--- C# COMPLETO Programação Orientada a Objetos + Projetos/Exercicios/Estrutura Condicional/Exercicio03/Exercicio03/Program.cs
+++ C# COMPLETO Programação Orientada a Objetos + Projetos/Exercicios/Estrutura Condicional/Exercicio03/Exercicio03/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Program {
+    internal class Program {
+        static void Main(string[] args) {
+            string[] vet = Console.ReadLine().Split(' ');
+
+            int num1 = int.Parse(vet[0]);
+            int num2 = int.Parse(vet[1]);
+
+            if (num1 % num2 == 0 || num2 % num1 == 0)
+                Console.WriteLine("Sao Multiplos");
+            else
+                Console.WriteLine("Nao Sao Multiplos");
+
+        }
+    }
+}
