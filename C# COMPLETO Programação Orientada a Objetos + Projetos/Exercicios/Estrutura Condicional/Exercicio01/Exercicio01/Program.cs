844247e1df88c1dc495edf381a95878b0dff9048
Júnior Veras <iramarjunhorveras@gmail.com>
2022-09-25 15:38:28 -0300

    Exercício 01

---
+++ C# COMPLETO Programação Orientada a Objetos + Projetos/Exercicios/Estrutura Condicional/Exercicio01/Exercicio01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Program
+{
+    internal class Program {
+        static void Main(string[] args) {
+            int a = int.Parse(Console.ReadLine());
+
+            if (a < 0) {
+                Console.WriteLine("NEGATIVO");
+            }
+            else if (a >= 0){
+                Console.WriteLine("NÃO NEGATIVO");
+            }
+            else {
+            }
+
+
+        }
+    }
+}
