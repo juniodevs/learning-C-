310ded06b4a21406c1bad9b8b8f047bc121d3a8a
Júnior Veras <iramarjunhorveras@gmail.com>
2022-09-25 15:55:39 -0300

    Exercício 05

---
+++ C# COMPLETO Programação Orientada a Objetos + Projetos/Exercicios/Estrutura Condicional/Exercicio05/Exercicio05/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Program {
+    internal class Program {
+        static void Main(string[] args) {
+            string[] valores = Console.ReadLine().Split(' ');
+
+            int produto = int.Parse(valores[0]);
+            int quantidade = int.Parse(valores[1]);
+            double preco = 0;
+
+            if (produto == 1)
+                preco = 4.00 * quantidade;
+            else if (produto == 2)
+                preco = 4.50 * quantidade;
+            else if (produto == 3)
+                preco = 5.00 * quantidade;
+            else if (produto == 4)
+                preco = 2.00 * quantidade;
+            else if (produto == 5)
+                preco = 1.50 * quantidade;
+            else
+                Console.WriteLine("PRODUTO ERRADO!");
+
+            Console.WriteLine( "Total: R$ "+ preco.ToString("F2"));
+
+        }
+    }
+}
