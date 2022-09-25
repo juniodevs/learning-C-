1c12aeaa250b8a3577a5d91854bff66b8fbed76f
Júnior Veras <iramarjunhorveras@gmail.com>
2022-09-25 15:38:33 -0300

    Exercício 02

---
+++ C# COMPLETO Programação Orientada a Objetos + Projetos/Exercicios/Estrutura Condicional/Exercicio02/Exercicio02/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Program
+{
+    internal class Program {
+        static void Main(string[] args) {
+            int num = int.Parse(Console.ReadLine());
+
+            if (num % 2 == 0)
+                Console.WriteLine("PAR");
+            else
+                Console.WriteLine("IMPAR");
+        }
+    }
+}
