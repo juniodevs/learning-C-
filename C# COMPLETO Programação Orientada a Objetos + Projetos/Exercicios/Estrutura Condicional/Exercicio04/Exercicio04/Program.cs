20e89a2d6ffce3f7e180261b26c253920a8a55fd
Júnior Veras <iramarjunhorveras@gmail.com>
2022-09-25 15:49:44 -0300

    Exercício 04

---
+++ C# COMPLETO Programação Orientada a Objetos + Projetos/Exercicios/Estrutura Condicional/Exercicio04/Exercicio04/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Leia a hora inicial e a hora final de um jogo. A seguir calcule a duração do jogo,
+ * sabendo que o mesmo pode começar em um dia e terminar em outro, tendo uma duração
+ * mínima de 1 hora e máxima de 24 horas.
+*/
+
+using System;
+
+namespace Program {
+    internal class Program {
+        static void Main(string[] args) {
+
+            string[] vet = Console.ReadLine().Split(' ');
+
+            int horainicio = int.Parse(vet[0]);
+            int horariofinal = int.Parse(vet[1]);
+
+            int duracao;
+            if (horainicio < horariofinal) {
+                duracao = horariofinal - horainicio;
+            }
+            else {
+                duracao = 24 - horainicio + horariofinal;
+            }
+            Console.WriteLine("O JOGO DUROU " + duracao + " HORA(S)");
+
+
+        }
+    }
+}
