6576b30fa15fecee3a898323601cba8b3aade2f0
Júnior Veras <iramarjunhorveras@gmail.com>
2022-09-26 16:13:14 -0300

    Exercício 01

---
+++ C# COMPLETO Programação Orientada a Objetos + Projetos/Exercicios/Estrutura Repetitiva WHILE/Exercicio01/Exercicio01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Mail;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program {
+        static void Main(string[] args) {
+
+            int senha = 0;
+
+            while (senha != 2002) {
+                senha = int.Parse(Console.ReadLine());
+
+                if (senha == 2002)
+                    Console.WriteLine("Acesso Permitido!");
+                else
+                    Console.WriteLine("Senha Invalida!");
+            }
+
+        }
+    }
+}
