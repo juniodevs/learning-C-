b0cffc01219d6afb41e5ad25f801e702bcc31ea0
Júnior Veras <iramarjunhorveras@gmail.com>
2022-09-26 16:25:40 -0300

    Exercício 03

---
+++ C# COMPLETO Programação Orientada a Objetos + Projetos/Exercicios/Estrutura Repetitiva WHILE/Exercicio03/Exercicio03/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Program
+{
+    internal class Program {
+        static void Main(string[] args) {
+            int num = 0;
+            int gasolina = 0;
+            int alcool = 0;
+            int disel = 0;
+            while (true) {
+                Console.WriteLine("Digite um Número de 1 a 4");
+                num = int.Parse(Console.ReadLine());
+
+                if (num == 1)
+                    alcool++;
+                else if (num == 2)
+                    gasolina++;
+                else if (num == 3)
+                    disel++;
+                else if (num == 4) { 
+                    Console.WriteLine("Muito Obrigado");
+                    Console.WriteLine("Alcool: " + alcool);
+                    Console.WriteLine("Gasolina: " + gasolina);
+                    Console.WriteLine("Diesel: " + disel);
+                    break;
+                }
+                else {
+                    Console.WriteLine("Código Invalido");
+                }
+            }
+        }
+    }
+}
