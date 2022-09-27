3c5719ee0ace32f59d247a5aa7cf127a075895c8
Júnior Veras <iramarjunhorveras@gmail.com>
2022-09-27 17:02:17 -0300

    Exercício 05

---
+++ C# COMPLETO Programação Orientada a Objetos + Projetos/Exercicios/Introdução à Programação Orientada a Objetos/Exercicio05/Exercicio05/Funcionario.cs
@@ -0,0 +1,15 @@
+﻿namespace Exercicio05 {
+    internal class Funcionario {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido() {
+            return (SalarioBruto - Imposto);
+        }
+
+        public void AumentarSalario(double porcentagem) {
+            SalarioBruto += (SalarioBruto * porcentagem);
+        }
+    }
+}
---
+++ C# COMPLETO Programação Orientada a Objetos + Projetos/Exercicios/Introdução à Programação Orientada a Objetos/Exercicio05/Exercicio05/Program.cs
@@ -0,0 +1,39 @@
+﻿using Exercicio05;
+using System;
+using System.Globalization;
+namespace Program
+{
+    internal class Program {
+        static void Main(string[] args) {
+
+            Funcionario funcionario = new Funcionario();
+
+
+            Console.Write("Nome: ");
+            funcionario.Nome = Console.ReadLine();
+
+            Console.Write("Salário bruto: ");
+            funcionario.SalarioBruto = double.Parse(Console.ReadLine());
+
+            Console.Write("Imposto ");
+            funcionario.Imposto = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Funcionário "
+                + funcionario.Nome
+                + ", $ "
+                + funcionario.SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+
+            double porcentagem = double.Parse(Console.ReadLine());
+            porcentagem = porcentagem * 0.01;
+
+            funcionario.AumentarSalario(porcentagem);
+
+            Console.WriteLine("Dados Atualizados: "
+                + funcionario.Nome
+                + ", $ "
+                + funcionario.SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
