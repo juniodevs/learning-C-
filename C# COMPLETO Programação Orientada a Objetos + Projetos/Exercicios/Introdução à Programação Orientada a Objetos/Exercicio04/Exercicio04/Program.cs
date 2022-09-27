970bcdf58f130b277de689341a729cf4435f1988
Júnior Veras <iramarjunhorveras@gmail.com>
2022-09-27 16:45:31 -0300

    Exercício 04

---
+++ C# COMPLETO Programação Orientada a Objetos + Projetos/Exercicios/Introdução à Programação Orientada a Objetos/Exercicio04/Exercicio04/Program.cs
@@ -0,0 +1,25 @@
+﻿using Exercicio04;
+using System;
+using System.Globalization;
+
+namespace Program
+{
+    internal class Program {
+        static void Main(string[] args) {
+
+            Retangulo retangulo = new Retangulo();
+
+            Console.WriteLine("Entre a largura e altura do retângulo");
+
+            retangulo.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            retangulo.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("\nAREA = " + retangulo.Area().ToString("F2", CultureInfo.InvariantCulture)
+                + "\nPERÍMETRO = "
+                + retangulo.Perimetro().ToString("F2", CultureInfo.InvariantCulture)
+                + "\nDIAGONAL = " + retangulo.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+
+
+
+        }
+    }
+}
---
+++ C# COMPLETO Programação Orientada a Objetos + Projetos/Exercicios/Introdução à Programação Orientada a Objetos/Exercicio04/Exercicio04/Retangulo.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercicio04 {
+    internal class Retangulo {
+
+        public double Largura;
+        public double Altura;
+
+        public double Area() {
+            return Largura * Altura;
+        }
+
+        public double Perimetro() {
+            return 2.0 * (Altura + Largura);
+        }
+
+        public double Diagonal() {
+            return Math.Sqrt(Math.Pow(Largura,2) + Math.Pow(Altura,2));
+        }
+
+    }
+}
