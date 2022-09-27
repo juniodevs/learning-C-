c532aa3062b34a832a8772f3152ca0da980d127c
Júnior Veras <iramarjunhorveras@gmail.com>
2022-09-27 14:49:34 -0300

    41. Criando um método para obtermos os benefícios de reaproveitamento e delegação

--- C# COMPLETO Programação Orientada a Objetos + Projetos/projetos/Course/Course/Program.cs
+++ C# COMPLETO Programação Orientada a Objetos + Projetos/projetos/Course/Course/Program.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Globalization;
-using System.Runtime.Serialization;
+﻿using System.Globalization;
 
-namespace Course
-{
+namespace Course {
     class Program {
         static void Main(string[] args) {
 
@@ -15,7 +12,7 @@ namespace Course
             Console.WriteLine("Entre com as Medidas do Triângulo X: ");
 
             x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            x.B= double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
             Console.WriteLine("Entre com as Medidas do Triângulo Y: ");
@@ -24,11 +21,8 @@ namespace Course
             y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            double p = (x.A + x.B + x.C) / 2;
-            double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
-
-            p = (y.A + y.B + y.C) / 2;
-            double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+            double areaX = x.Area();
+            double areaY = y.Area();
 
             Console.WriteLine("Área de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
             Console.WriteLine("Área de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
--- C# COMPLETO Programação Orientada a Objetos + Projetos/projetos/Course/Course/Triangulo.cs
+++ C# COMPLETO Programação Orientada a Objetos + Projetos/projetos/Course/Course/Triangulo.cs
@@ -1,9 +1,17 @@
-﻿namespace Course {
+﻿using System;
+
+namespace Course {
     class Triangulo {
 
         public double A;
         public double B;
         public double C;
+
+        public double Area() {
+
+            double p = (A + B + C) / 2;
+            return  Math.Sqrt(p * (p - A) * (p - B) * (p - C)); ;
+        }
 
 
     }
