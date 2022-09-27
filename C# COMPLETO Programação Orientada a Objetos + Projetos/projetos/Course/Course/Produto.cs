4ecf3331da446831c9e80590a82ef3f45b1a6049
Júnior Veras <iramarjunhorveras@gmail.com>
2022-09-27 16:16:56 -0300

    42. Começando a resolver um segundo problema exemplo

---
+++ C# COMPLETO Programação Orientada a Objetos + Projetos/projetos/Course/Course/Produto.cs
@@ -0,0 +1,13 @@
+﻿namespace Course {
+    class Produto {
+        public string Nome;
+        public double preco;
+        public int quantidade;
+
+        public double ValorTotalEmEstoque() {
+
+            return preco * quantidade;
+        }
+
+    }
+}
--- C# COMPLETO Programação Orientada a Objetos + Projetos/projetos/Course/Course/Program.cs
+++ C# COMPLETO Programação Orientada a Objetos + Projetos/projetos/Course/Course/Program.cs
@@ -4,34 +4,50 @@ namespace Course {
     class Program {
         static void Main(string[] args) {
 
-            Triangulo x, y;
+            Produto p = new Produto();
 
-            x = new Triangulo();
-            y = new Triangulo();
+            Console.WriteLine("Entre os dados do produto: ");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            p.quantidade = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Entre com as Medidas do Triângulo X: ");
+            Console.WriteLine("Dados do Produto: " + p);
 
-            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            Console.WriteLine("Entre com as Medidas do Triângulo Y: ");
-
-            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            double areaX = x.Area();
-            double areaY = y.Area();
-
-            Console.WriteLine("Área de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
-            Console.WriteLine("Área de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
-
-            if (areaX > areaY)
-                Console.WriteLine("Maior área: X");
-            else
-                Console.WriteLine("Maior área Y");
         }
+
+
+
+        //    Triangulo x, y;
+
+        //    x = new Triangulo();
+        //    y = new Triangulo();
+
+        //    Console.WriteLine("Entre com as Medidas do Triângulo X: ");
+
+        //    x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        //    x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        //    x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        //    Console.WriteLine("Entre com as Medidas do Triângulo Y: ");
+
+        //    y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        //    y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        //    y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        //    double areaX = x.Area();
+        //    double areaY = y.Area();
+
+        //    Console.WriteLine("Área de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+        //    Console.WriteLine("Área de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+        //    if (areaX > areaY)
+        //        Console.WriteLine("Maior área: X");
+        //    else
+        //        Console.WriteLine("Maior área Y");
+        //}
 
 
         //Console.Write("Digite Quantos Números inteiros você vai digitar: ");
