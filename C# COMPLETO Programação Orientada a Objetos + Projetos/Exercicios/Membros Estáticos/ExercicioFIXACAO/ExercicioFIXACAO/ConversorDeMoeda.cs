00af106dfd02787a55abc3b1d9b03f88b93beb89
Júnior Veras <iramarjunhorveras@gmail.com>
2022-10-01 13:11:58 -0300

    48. Exercícios propostos (membros estáticos)

---
+++ C# COMPLETO Programação Orientada a Objetos + Projetos/Exercicios/Membros Estáticos/ExercicioFIXACAO/ExercicioFIXACAO/ConversorDeMoeda.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Globalization;
+
+namespace ExercicioFIXACAO {
+    class ConversorDeMoeda {
+        public static double Converter() {
+
+        Console.Write("Qual é a cotação do dólar? ");
+        double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.Write("Quantos Dólares você vai comprar? ");
+        double dolaresCompra = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            return (cotacao * dolaresCompra) + (((cotacao * dolaresCompra) * 0.06));
+        }
+
+    }
+
+}
---
+++ C# COMPLETO Programação Orientada a Objetos + Projetos/Exercicios/Membros Estáticos/ExercicioFIXACAO/ExercicioFIXACAO/Program.cs
@@ -0,0 +1,13 @@
+﻿using ExercicioFIXACAO;
+using System;
+using System.Globalization;
+
+namespace Program
+{
+    internal class Program {
+        static void Main(string[] args) {
+
+        Console.WriteLine("Valor a ser pago em reais = " + ConversorDeMoeda.Converter().ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
