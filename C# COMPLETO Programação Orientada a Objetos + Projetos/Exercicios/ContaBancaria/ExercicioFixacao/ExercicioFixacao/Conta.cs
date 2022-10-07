715bc927e45a444f163592ae5c0fc9e01c9ac2ca
Júnior Veras <iramarjunhorveras@gmail.com>
2022-10-07 17:46:01 -0300

    60. Exercício de fixação

---
+++ C# COMPLETO Programação Orientada a Objetos + Projetos/Exercicios/ContaBancaria/ExercicioFixacao/ExercicioFixacao/Conta.cs
@@ -0,0 +1,53 @@
+﻿
+using System.Globalization;
+
+namespace ExercicioFixacao {
+    class Conta {
+        private int _numConta = 0;
+        private string _titularDaConta;
+        private double _saldo = 0;
+
+        public Conta(int numeroConta, string nome) {
+            this._numConta = numeroConta;
+            this._titularDaConta = nome;
+        }
+
+        public void depositoInicial() {
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char verificacao = char.Parse(Console.ReadLine());
+            if (verificacao == 's' || verificacao == 'S') {
+                Console.Write("Entre o valor de depósito inicial: ");
+                _saldo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+        }
+
+        public void deposito() {
+
+            Console.Write("Entre o valor do depósito: ");
+            double deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            _saldo += deposito;
+
+            dadosDaConta();
+        }
+
+        public void saque() {
+
+            Console.Write("Entre o valor do saque: ");
+            double saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            _saldo -= (saque + 5);
+
+            dadosDaConta();
+        }
+
+        public void dadosDaConta(){
+            Console.WriteLine("Dados da conta:\nConta "
+                + _numConta
+                + ", Titular: "
+                + _titularDaConta
+                + ", Saldo: R$" + _saldo.ToString("F2", CultureInfo.InvariantCulture) + "\n");
+        }
+
+    }
+}
---
+++ C# COMPLETO Programação Orientada a Objetos + Projetos/Exercicios/ContaBancaria/ExercicioFixacao/ExercicioFixacao/Program.cs
@@ -0,0 +1,19 @@
+﻿using ExercicioFixacao;
+using System;
+
+namespace Program
+{
+    internal class Program {
+        static void Main(string[] args) {
+            Console.Write("Entre o número da conta: ");
+            int numeroConta = int.Parse(Console.ReadLine());
+            Console.Write("Entre o Titular da conta: ");
+            string nome = Console.ReadLine();
+            Conta pessoa1 = new Conta(numeroConta, nome);
+            pessoa1.depositoInicial();
+            pessoa1.dadosDaConta();
+            pessoa1.deposito();
+            pessoa1.saque();
+        }
+    }
+}
