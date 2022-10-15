edf19b608d1b50f60f2d8af33aa7734fe38d7d1a
Júnior Veras <iramarjunhorveras@gmail.com>
2022-10-15 11:06:41 -0300

    91. Propriedades e operações com DateTime

--- C# COMPLETO Programação Orientada a Objetos + Projetos/projetos/Course/Course/Program.cs
+++ C# COMPLETO Programação Orientada a Objetos + Projetos/projetos/Course/Course/Program.cs
@@ -1,39 +1,86 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Globalization;
 
 namespace Course {
     class Program {
         static void Main(string[] args) {
 
-            TimeSpan t1 = new TimeSpan();
-            TimeSpan t2 = new TimeSpan(900000000L);
-            TimeSpan t3 = new TimeSpan(2, 11, 21);
-            TimeSpan t4 = new TimeSpan(1, 2, 11, 21);
-            TimeSpan t5 = new TimeSpan(1, 2, 11, 21, 321);
+            DateTime d = new DateTime(2001, 8, 15, 13, 45, 58, 215);
+            string s1 = d.ToLongDateString();
+            string s2 = d.ToLongTimeString();
+            string s3 = d.ToShortDateString();
+            string s4 = d.ToShortTimeString();
+            string s5 = d.ToString();
+            string s6 = d.ToString("yyy-MM-dd HH:mm:ss");
+            string s7 = d.ToString("yyy-MM-dd HH:mm:ss.fff");
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            Console.WriteLine(s3);
+            Console.WriteLine(s4);
+            Console.WriteLine(s5);
+            Console.WriteLine(s6);
+            Console.WriteLine(s7);
 
-            Console.WriteLine(t1);
-            Console.WriteLine(t2);
-            Console.WriteLine(t3);
-            Console.WriteLine(t4);
-            Console.WriteLine(t5);
+            Console.WriteLine(d);
+            Console.WriteLine("1) Date: " + d.Date);
+            Console.WriteLine("2) Day " + d.Day);
+            Console.WriteLine("3) DayOfWeek " + d.DayOfWeek);
+            Console.WriteLine("4) DayOfYear: " + d.DayOfYear);
+            Console.WriteLine("5) Hour: " + d.Hour);
+            Console.WriteLine("6) Kind " + d.Kind);
+            Console.WriteLine("7) Millisecond: " + d.Millisecond);
+            Console.WriteLine("8) Minute: " + d.Minute);
+            Console.WriteLine("9) Month: " + d.Month);
+            Console.WriteLine("10) Second: " + d.Second);
+            Console.WriteLine("11) Ticks: " + d.Ticks);
+            Console.WriteLine("12) TimeOfDay " + d.TimeOfDay);
+            Console.WriteLine("13) Year " + d.Year);
 
-            TimeSpan timeFromDays = TimeSpan.FromDays(1.5);
-            TimeSpan timeFromHours = TimeSpan.FromHours(1.5);
-            TimeSpan timeFromMinutes = TimeSpan.FromMinutes(1.5);
-            TimeSpan timeFromSeconds = TimeSpan.FromSeconds(1.5);
-            TimeSpan timeFromMillieseconds = TimeSpan.FromMilliseconds(1.5);
-            TimeSpan timeFromTicks = TimeSpan.FromTicks(900000000L);
-            Console.WriteLine(timeFromDays);
-            Console.WriteLine(timeFromHours);
-            Console.WriteLine(timeFromMinutes);
-            Console.WriteLine(timeFromSeconds);
-            Console.WriteLine(timeFromMillieseconds);
-            Console.WriteLine(timeFromTicks);
+            DateTime dd = new DateTime(2001, 8, 15, 13, 45, 58, 215);
+            DateTime d2 = dd.AddHours(2);
+            DateTime d3 = dd.AddMinutes(3);
+            DateTime d4 = dd.AddDays(7);
+            Console.WriteLine(dd);
+            Console.WriteLine(d2);
+            Console.WriteLine(d3);
+            Console.WriteLine(d4);
+
+            DateTime sub1 = new DateTime(2000, 10, 15);
+            DateTime sub2 = new DateTime(2000, 10, 18);
+
+            TimeSpan t = sub2.Subtract(sub1);
+
+            Console.WriteLine(t);
+          
         }
     }
 }
 
+//TimeSpan t1 = new TimeSpan();
+//TimeSpan t2 = new TimeSpan(900000000L);
+//TimeSpan t3 = new TimeSpan(2, 11, 21);
+//TimeSpan t4 = new TimeSpan(1, 2, 11, 21);
+//TimeSpan t5 = new TimeSpan(1, 2, 11, 21, 321);
 
+//Console.WriteLine(t1);
+//Console.WriteLine(t2);
+//Console.WriteLine(t3);
+//Console.WriteLine(t4);
+//Console.WriteLine(t5);
+
+//TimeSpan timeFromDays = TimeSpan.FromDays(1.5);
+//TimeSpan timeFromHours = TimeSpan.FromHours(1.5);
+//TimeSpan timeFromMinutes = TimeSpan.FromMinutes(1.5);
+//TimeSpan timeFromSeconds = TimeSpan.FromSeconds(1.5);
+//TimeSpan timeFromMillieseconds = TimeSpan.FromMilliseconds(1.5);
+//TimeSpan timeFromTicks = TimeSpan.FromTicks(900000000L);
+//Console.WriteLine(timeFromDays);
+//Console.WriteLine(timeFromHours);
+//Console.WriteLine(timeFromMinutes);
+//Console.WriteLine(timeFromSeconds);
+//Console.WriteLine(timeFromMillieseconds);
+//Console.WriteLine(timeFromTicks);
 
 //DateTime dataexact = DateTime.ParseExact("2000-08-15", "yyyy-MM-dd", CultureInfo.InvariantCulture);
 //Console.WriteLine(dataexact);
