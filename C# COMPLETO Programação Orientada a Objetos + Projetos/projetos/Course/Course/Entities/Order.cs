ee671237ef6fd48dc250407e7357a5c19be323d5
Júnior Veras <iramarjunhorveras@gmail.com>
2022-10-17 17:55:41 -0300

    124. Enumerações

---
+++ C# COMPLETO Programação Orientada a Objetos + Projetos/projetos/Course/Course/Entities/Order.cs
@@ -0,0 +1,19 @@
+﻿namespace Course.Entities
+{
+    class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public Enums.OrderStatus Status { get; set; }
+
+
+        public override string ToString()
+        {
+            return Id
+                + ", "
+                + Moment
+                + ", "
+                + Status;
+        }
+    }
+}
--- C# COMPLETO Programação Orientada a Objetos + Projetos/projetos/Course/Course/Program.cs
+++ C# COMPLETO Programação Orientada a Objetos + Projetos/projetos/Course/Course/Program.cs
@@ -1,10 +1,31 @@
-﻿using System;
+﻿using Course.Entities;
+using Course.Entities.Enums;
+using System;
 using System.Collections;
 using System.Globalization;
 
-namespace Course {
-    class Program {
-        static void Main(string[] args) {
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+
+            string txt = OrderStatus.PendingPayment.ToString();
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(os);
+            Console.WriteLine(txt);
+            
 
         }
     }
