992f288aaab753d3aa93d23b12fdeed3dda000bb
Júnior Veras <iramarjunhorveras@gmail.com>
2022-10-24 10:33:21 -0300

    129. Exercício resolvido 01 - PARTE 2/3

---
+++ C# COMPLETO Programação Orientada a Objetos + Projetos/Exercicios/Enums/ExercicioFixacao/ExercicioFixacao/Entities/Department.cs
@@ -0,0 +1,14 @@
+﻿namespace ExercicioFixacao.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+        public Department()
+        {
+        }
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ C# COMPLETO Programação Orientada a Objetos + Projetos/Exercicios/Enums/ExercicioFixacao/ExercicioFixacao/Entities/Worker.cs
@@ -0,0 +1,52 @@
+﻿using ExercicioFixacao.Entities.Enums;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+
+namespace ExercicioFixacao.Entities
+{
+    class Worker
+    {
+        public String Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>();
+
+        public Worker()
+        {
+        }
+
+        public Worker(string name, WorkerLevel level, double baseSalary, Department department)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        public void AddContract(HourContract contract)
+        {
+            Contracts.Add(contract);
+        }
+
+        public void RemoveContract (HourContract contract)
+        {
+            Contracts.Remove(contract);
+        }
+
+        public double Income(int year, int month)
+        {
+            double sum = BaseSalary;
+            foreach(HourContract contract in Contract)
+            {
+                if (contract.Date.Year == year && contract.Date.Month == month)
+                {
+                    sum += contract.TotalValue();
+                }
+            }
+
+            return sum;
+        }
+
+    }
+}
