c6e336caea9d8468311f7ca3594899cf1175168b
Júnior Veras <iramarjunhorveras@gmail.com>
2022-10-24 15:43:39 -0300

    132. Exercício proposto (Enumerações e Composição)

---
+++ C# COMPLETO Programação Orientada a Objetos + Projetos/Exercicios/Enums/ExercicioFixacaoEnums/ExercicioFixacaoEnums/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace ExercicioFixacaoEnums.Entities.Enums
+{
+    enum OrderStatus : int
+    {
+        Peding_Payment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+    }
+}
