61bd0e8ab839f3758f1d82ae2c8f3dec62ce3b23
Júnior Veras <iramarjunhorveras@gmail.com>
2022-11-03 09:22:14 -0300

    143. Classes abstratas

--- C# COMPLETO Programação Orientada a Objetos + Projetos/projetos/Course/Course/Program.cs
+++ C# COMPLETO Programação Orientada a Objetos + Projetos/projetos/Course/Course/Program.cs
@@ -10,17 +10,39 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            Account acc1 = new Account(1001, "Alex", 500.0);
-            Account acc2 = new SavingsAccount(1002, "Anna", 500.0, 0.01);
+            List<Account> list = new List<Account>();
 
-            acc1.Withdraw(10.0);
-            acc2.Withdraw(10.0);
+            list.Add(new SavingsAccount(1001, "Alex", 500.0, 0.01));
+            list.Add(new BusinessAccount(1002, "Maria", 500.0, 400));
+            list.Add(new SavingsAccount(1003, "Bob", 500.0, 0.01));
+            list.Add(new BusinessAccount(1004, "Ana", 500.0, 500.0));
 
-            Console.WriteLine(acc1.Balance);
-            Console.WriteLine(acc2.Balance);
+            double sum = 0.0;
+
+            foreach(Account acc in list)
+            {
+                sum += acc.Balance;
+            }
+
+            Console.WriteLine("Total Balance: " + sum.ToString("F2",CultureInfo.InvariantCulture));
+
+            foreach(Account acc in list)
+            {
+                acc.Withdraw(10.0);
+            }
+
+            foreach(Account acc in list)
+            {
+                Console.WriteLine("Updated balance for account"
+                    + acc.Number
+                    + ": "
+                    + acc.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            }
         }
     }
 }
+
+
 
 //Account acc = new Account(1001, "Alex", 0.0);
 //BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
