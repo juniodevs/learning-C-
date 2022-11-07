f629fdb1b9ca90622ef16de6c86c5be4b9c450d1
Júnior Veras <iramarjunhorveras@gmail.com>
2022-11-07 16:32:36 -0300

    Exercicio Proposto

---
+++ C# COMPLETO Programação Orientada a Objetos + Projetos/Exercicios/Execoes/ExercicioProp/ExercicioProp/Entities/Account.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using ExercicioProp.Entities.Exceptions;
+
+
+namespace ExercicioProp.Entities
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithdrawLimit { get; set; }
+        public Account()
+        {
+        }
+        public Account(int number, string holder, double balance, double withdrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithdrawLimit = withdrawLimit;
+        }
+
+        public void Deposit(double amount)
+        {
+            if(amount < 0)
+            {
+                throw new DomainException("the value cannot be less than 0");
+            }
+            Balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if(amount < 0.0)
+            {
+                throw new DomainException("the value cannot be less than 0");
+            }
+            if (WithdrawLimit < amount)
+            {
+                throw new DomainException("The amount exceeds withdraw limit");
+            }
+            if (amount > Balance)
+            {
+                throw new DomainException("The value cannot be greater than the balance");
+            }
+            Balance -= amount;
+        }
+
+    }
+}
---
+++ C# COMPLETO Programação Orientada a Objetos + Projetos/Exercicios/Execoes/ExercicioProp/ExercicioProp/Program.cs
@@ -0,0 +1,47 @@
+﻿using ExercicioProp.Entities;
+using ExercicioProp.Entities.Exceptions;
+using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try {
+            Console.WriteLine("Enter account data");
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Holder: ");
+            string holder = Console.ReadLine();
+            Console.Write("Initial balance: ");
+            double initialbalance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Withdraw limit: ");
+            double withdrawlimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Account account = new Account(number, holder, withdrawlimit, initialbalance);
+
+            Console.Write("Enter amount for withdraw: ");
+            double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            account.Withdraw(amount);
+            }
+            catch(DomainException e)
+            {
+                Console.WriteLine("Error!: " + e.Message);
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine("Error!: " + e.Message);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error!: " + e.Message);
+            }
+            
+
+        }
+    }
+}
