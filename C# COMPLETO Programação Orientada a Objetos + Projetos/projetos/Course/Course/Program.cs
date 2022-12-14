5333082056e13ed28f3326f2583b3149a6d709b5
Júnior Veras <iramarjunhorveras@gmail.com>
2022-12-14 11:15:33 -0300

    197. Bloco using

--- C# COMPLETO Programação Orientada a Objetos + Projetos/projetos/Course/Course/Program.cs
+++ C# COMPLETO Programação Orientada a Objetos + Projetos/projetos/Course/Course/Program.cs
@@ -12,31 +12,53 @@ namespace Course
         static void Main(string[] args)
         {
             string path = @"C:\Windows\Temp\file1.txt";
-            FileStream fs = null;
-            StreamReader sr = null;
 
-            try{
-                fs = new FileStream(path, FileMode.Open);
-                sr = new StreamReader(fs);
-                string line = sr.ReadLine();
-                Console.WriteLine(line);
-
-
+            try
+                { 
+            using (FileStream fs = new FileStream(path, FileMode.Open))
+            {
+                using (StreamReader sr = new StreamReader(fs))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+            }
             }
             catch(IOException e)
             {
-                Console.WriteLine("An errorr ocurred");
+                Console.WriteLine("An error occurred");
                 Console.WriteLine(e.Message);
             }
-            finally
-            {
-                if (sr != null) sr.Close();
-                if (fs != null) fs.Close();
-            }
-        }
 
+
+
+        }
     }
 }
+
+//string path = @"C:\Windows\Temp\file1.txt";
+//FileStream fs = null;
+//StreamReader sr = null;
+
+//try{
+//    fs = new FileStream(path, FileMode.Open);
+//    sr = new StreamReader(fs);
+//    string line = sr.ReadLine();
+//    Console.WriteLine(line);
+//}
+//catch(IOException e)
+//{
+//    Console.WriteLine("An errorr ocurred");
+//    Console.WriteLine(e.Message);
+//}
+//finally
+//{
+//    if (sr != null) sr.Close();
+//    if (fs != null) fs.Close();
+//}
 
 //string sourcePath = @"c:\Windows\Temp\file1.txt";
 //string targetPath = @"c:\Windows\Temp\file2.txt";
