7088396b9f46e1ceeac0c826fc3297109a0f271e
Júnior Veras <iramarjunhorveras@gmail.com>
2022-12-14 10:44:28 -0300

    195. File, FileInfo e IOException

--- C# COMPLETO Programação Orientada a Objetos + Projetos/projetos/Course/Course/Program.cs
+++ C# COMPLETO Programação Orientada a Objetos + Projetos/projetos/Course/Course/Program.cs
@@ -12,6 +12,25 @@ namespace Course
         static void Main(string[] args)
         {
 
+            string sourcePath = @"c:\Windows\Temp\file1.txt";
+            string targetPath = @"c:\Windows\Temp\file2.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An Error Occurred");
+                Console.WriteLine(e.Message);
+            }
+
         }
 
     }
