c05b83849dd9b5b80617f67bd3df8ad324cb700f
Júnior Veras <iramarjunhorveras@gmail.com>
2022-12-14 11:11:40 -0300

    196. FileStream e StreamReader

--- C# COMPLETO Programação Orientada a Objetos + Projetos/projetos/Course/Course/Program.cs
+++ C# COMPLETO Programação Orientada a Objetos + Projetos/projetos/Course/Course/Program.cs
@@ -11,30 +11,51 @@ namespace Course
     {
         static void Main(string[] args)
         {
+            string path = @"C:\Windows\Temp\file1.txt";
+            FileStream fs = null;
+            StreamReader sr = null;
 
-            string sourcePath = @"c:\Windows\Temp\file1.txt";
-            string targetPath = @"c:\Windows\Temp\file2.txt";
+            try{
+                fs = new FileStream(path, FileMode.Open);
+                sr = new StreamReader(fs);
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
 
-            try
-            {
-                FileInfo fileInfo = new FileInfo(sourcePath);
-                fileInfo.CopyTo(targetPath);
-                string[] lines = File.ReadAllLines(sourcePath);
-                foreach (string line in lines)
-                {
-                    Console.WriteLine(line);
-                }
+
             }
-            catch (IOException e)
+            catch(IOException e)
             {
-                Console.WriteLine("An Error Occurred");
+                Console.WriteLine("An errorr ocurred");
                 Console.WriteLine(e.Message);
             }
-
+            finally
+            {
+                if (sr != null) sr.Close();
+                if (fs != null) fs.Close();
+            }
         }
 
     }
 }
+
+//string sourcePath = @"c:\Windows\Temp\file1.txt";
+//string targetPath = @"c:\Windows\Temp\file2.txt";
+
+//try
+//{
+//    FileInfo fileInfo = new FileInfo(sourcePath);
+//    fileInfo.CopyTo(targetPath);
+//    string[] lines = File.ReadAllLines(sourcePath);
+//    foreach (string line in lines)
+//    {
+//        Console.WriteLine(line);
+//    }
+//}
+//catch (IOException e)
+//{
+//    Console.WriteLine("An Error Occurred");
+//    Console.WriteLine(e.Message);
+//}
 
 //FileStream fs = null;
 //try
