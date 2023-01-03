1ae88d338ff2226edc74ec3e4671d0fde125d711
Júnior Veras <iramarjunhorveras@gmail.com>
2023-01-03 10:29:35 -0300

    204. Solução sem interface - PARTE 1

---
+++ C# COMPLETO Programação Orientada a Objetos + Projetos/Exercicios/SemInterface/SemInterface/Program.cs
@@ -0,0 +1,22 @@
+﻿using SemInterface.Entities;
+using System;
+using System.Globalization;
+
+namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Rental Data ");
+            Console.Write("Car Moddel: ");
+            string Model = Console.ReadLine();
+            Console.Write("Pickup (dd/MM/yyyy hh:mm: ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            Console.Write("Return (dd/MM/yyyy hh:mm: ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            CarRental carRetal = new CarRental(start, finish, new Vehicle(Model));
+        }
+    }
+}
