10a9a87574a2fbe89aacb2c21d2b1d31f82e5b4b
Júnior Veras <iramarjunhorveras@gmail.com>
2023-01-16 15:30:01 -0300

    UPDATE

--- C# COMPLETO Programação Orientada a Objetos + Projetos/projetos/Course/Course/Program.cs
+++ C# COMPLETO Programação Orientada a Objetos + Projetos/projetos/Course/Course/Program.cs
@@ -12,21 +12,23 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            string path = @"C:\Windows\Temp\myfolder\file1.txt";
 
-            Console.WriteLine("DirectorySeparatorChar: " + Path.DirectorySeparatorChar);
-            Console.WriteLine("PathSeparator: " + Path.PathSeparator);
-            Console.WriteLine("GetDirectoryName: " + Path.GetDirectoryName(path));
-            Console.WriteLine("GetFileName: " + Path.GetFileName(path));
-            Console.WriteLine("GetFileNameWithoutExtension: " + Path.GetFileNameWithoutExtension(path));
-            Console.WriteLine("GetExtension: " + Path.GetExtension(path));
-            Console.WriteLine("GetFullPath: " + Path.GetFullPath(path));
-            Console.WriteLine("GetTempPath: " + Path.GetTempPath());
         }
     }
 }
 
-            //    string path = @"C:\Windows\Temp\myfolder";
+//string path = @"C:\Windows\Temp\myfolder\file1.txt";
+
+//Console.WriteLine("DirectorySeparatorChar: " + Path.DirectorySeparatorChar);
+//Console.WriteLine("PathSeparator: " + Path.PathSeparator);
+//Console.WriteLine("GetDirectoryName: " + Path.GetDirectoryName(path));
+//Console.WriteLine("GetFileName: " + Path.GetFileName(path));
+//Console.WriteLine("GetFileNameWithoutExtension: " + Path.GetFileNameWithoutExtension(path));
+//Console.WriteLine("GetExtension: " + Path.GetExtension(path));
+//Console.WriteLine("GetFullPath: " + Path.GetFullPath(path));
+//Console.WriteLine("GetTempPath: " + Path.GetTempPath());
+
+//    string path = @"C:\Windows\Temp\myfolder";
 
 //    try{
 
