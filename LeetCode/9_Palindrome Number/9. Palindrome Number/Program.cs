66459e539f4bada678f94414bf2ba013d183acf1
Júnior Veras <iramarjunhorveras@gmail.com>
2023-01-16 16:24:58 -0300

    9. Palindrome Number

---
+++ LeetCode/9_Palindrome Number/9. Palindrome Number/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ LeetCode/9_Palindrome Number/9. Palindrome Number/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace _9._Palindrome_Number
+{
+    public class Solution
+    {
+        public bool IsPalindrome(int x)
+        {
+            String textX = x.ToString();
+            String textY = new string(textX.Reverse().ToArray());
+
+            if (textX == textY)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
