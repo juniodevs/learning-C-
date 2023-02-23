1e68b5a9243950f7ea682eb836a91f462339b054
Júnior Veras <iramarjunhorveras@gmail.com>
2023-02-23 10:56:59 -0300

    UPDATE

---
+++ C# COMPLETO Programação Orientada a Objetos + Projetos/Exercicios/ComInterface/14. Longest Common Prefix/14. Longest Common Prefix/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strs = { "flower", "flow", "flight" };
+
+            while(true)
+            {
+                if ()
+            }
+        }
+    }
+}
---
+++ C# COMPLETO Programação Orientada a Objetos + Projetos/projetos/Course/Course/Entities/Pessoa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.Entities
+{
+    internal class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        public Pessoa(string nome, int idade)
+        {
+            Nome = nome;
+            Idade = idade;
+        }
+
+        public void Apresentar()
+        {
+            Console.WriteLine("Oi eu sou " + Nome + " Tenho " + Idade + " Anos de Idade");
+        }
+    }
+}
