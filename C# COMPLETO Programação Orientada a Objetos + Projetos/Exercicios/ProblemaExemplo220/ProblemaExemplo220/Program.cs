9901249a61cb601d2f08153ccbfdb9cca5de38c3
Júnior Veras <iramarjunhorveras@gmail.com>
2023-03-24 14:35:40 -0300

    220. Exercício resolvido sobre conjuntos

---
+++ C# COMPLETO Programação Orientada a Objetos + Projetos/Exercicios/ProblemaExemplo220/ProblemaExemplo220/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using ProblemaExemplo220.Entities;
+namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<LogRecord> set = new HashSet<LogRecord>();
+
+            Console.Write("Enter File full path: ");
+            string path = @"C:\Windows\Temp\in.txt";
+            Console.WriteLine(path);
+            
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while(!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(' ');
+                        string name = line[0];
+                        DateTime instante = DateTime.Parse(line[1]);
+
+                        set.Add(new LogRecord(name, instante));
+                    }
+                    Console.Write("Total users: " + set.Count);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+
+            
+
+
+        }
+    }
+}
