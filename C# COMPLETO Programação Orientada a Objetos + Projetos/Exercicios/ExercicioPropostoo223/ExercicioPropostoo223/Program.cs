1ea6b282045c6fb040edd6a22174eafbf199983c
Júnior Veras <iramarjunhorveras@gmail.com>
2023-03-24 15:39:26 -0300

    223. Exercício proposto sobre Dictionary

---
+++ C# COMPLETO Programação Orientada a Objetos + Projetos/Exercicios/ExercicioPropostoo223/ExercicioPropostoo223/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+
+namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> candidatos = new Dictionary<string, int>();
+
+            string path = @"C:\Windows\Temp\in.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(',');
+
+                        string candidate = line[0];
+                        int votos = int.Parse(line[1]);
+
+                        if (candidatos.ContainsKey(candidate))
+                        {
+                            candidatos[candidate] += votos;
+                        }
+                        else
+                        {
+                            candidatos[candidate] = votos;
+                        }
+                    }
+                    foreach (var item in candidatos)
+                    {
+                        Console.WriteLine(item.Key + ": " + item.Value);
+
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+        }
+    }
+}
