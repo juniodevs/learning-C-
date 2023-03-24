c3035285dbb92c7fc828b9d5d6a36654a5617318
Júnior Veras <iramarjunhorveras@gmail.com>
2023-03-24 14:47:24 -0300

    221. Exercício proposto sobre conjuntos

---
+++ C# COMPLETO Programação Orientada a Objetos + Projetos/Exercicios/ExercicioProposto221/ExercicioProposto221/Entities/Studant.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioProposto221.Entities
+{
+    class Studant
+    {
+        public int StudantId { get; set; }
+
+        public Studant(int studantId)
+        {
+            StudantId = studantId;
+        }
+
+        public override int GetHashCode()
+        {
+            return StudantId.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(!(obj is Studant))
+            return false;
+            Studant other = obj as Studant;
+            return StudantId.Equals(other.StudantId);
+        }
+    }
+}
---
+++ C# COMPLETO Programação Orientada a Objetos + Projetos/Exercicios/ExercicioProposto221/ExercicioProposto221/Program.cs
@@ -0,0 +1,43 @@
+﻿using ExercicioProposto221.Entities;
+using System;
+
+namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Studant> set = new HashSet<Studant>();
+
+            Console.Write("How Many students for courrse A? ");
+            int a = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < a; i++)
+            {
+                int ID = int.Parse(Console.ReadLine());
+                set.Add(new Studant(ID));
+            }
+
+            Console.Write("How Many students for courrse B? ");
+            int b = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < b; i++)
+            {
+                int ID = int.Parse(Console.ReadLine());
+                set.Add(new Studant(ID));
+            }
+
+            Console.Write("How Many students for courrse C? ");
+            int c = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < c; i++)
+            {
+                int ID = int.Parse(Console.ReadLine());
+                set.Add(new Studant(ID));
+            }
+
+            Console.WriteLine("Total Students: " + set.Count);
+
+        }
+    }
+}
