994a02a61a8ede310e8d014f66726a0bc261a636
Júnior Veras <iramarjunhorveras@gmail.com>
2023-03-27 13:38:00 -0300

    231. Multicast delegates

--- C# COMPLETO Programação Orientada a Objetos + Projetos/projetos/Course/Course/Program.cs
+++ C# COMPLETO Programação Orientada a Objetos + Projetos/projetos/Course/Course/Program.cs
@@ -14,23 +14,32 @@ using Course.Services;
 namespace Course
 {
 
-    delegate double BinaryNumericOperation(double n1, double n2);
-    class ProgramS
+    delegate void BinaryNumericOperation(double n1, double n2);
+    class Program
     {
         static void Main(string[] args)
         {
+
             double a = 10;
             double b = 12;
 
-            BinaryNumericOperation op = CalculationServices.Sum;
+            BinaryNumericOperation op = CalculationServices.ShowSum;
+            op += CalculationServices.ShowMax;
 
-            double result = op(a, b);
- 
-            Console.WriteLine(result);
+            op(a, b);
 
         }
     }
 }
+
+//double a = 10;
+//double b = 12;
+
+//BinaryNumericOperation op = CalculationServices.Sum;
+
+//double result = op(a, b);
+
+//Console.WriteLine(result);
 
 //List<ProductComparison> list = new List<ProductComparison>();
 
